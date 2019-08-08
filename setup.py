aa061f26ce88c0b4ca81c8d20337ae7a0950a085
Mark Jordan <mjordan@sfu.ca>
2019-08-07 17:17:25 -0700

    Work on #7.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="Islandora Workbench",
+    version="master",
+    author="Mark Jordan",
+	author_email="mjordan@sfu",
+    description="A command-line tool that allows creation, updating, and deletion of Islandora 8 content.",
+    url="https://github.com/mjordan/islandora_workbench",
+	license="The Unlicense",
+    install_requires=['requests>=2.22,<3', 'ruamel.yaml'],
+    python_requires='>=3'
+)
