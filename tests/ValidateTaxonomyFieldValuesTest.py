d69cdc375fcc8fb4d861e556ecdb8450ac3fc100
Mark Jordan <mjordan@sfu.ca>
2020-03-31 21:37:08 -0700

    Addition to ValidateTaxonomyFieldValuesTest.py.

--- tests/ValidateTaxonomyFieldValuesTest.py
+++ tests/ValidateTaxonomyFieldValuesTest.py
@@ -12,12 +12,12 @@ class ValidateTaxonomyFieldValuesTest(unittest.TestCase):
         output = subprocess.check_output(create_cmd)
         self.output = output.decode().strip()
 
-    def test_create_check(self):
+    def test_validate_term_ids(self):
         lines = self.output.splitlines()
         # We don't test for "OK, term IDs in CSV file exist in their respective taxonomies."
         # because the target Islandora might not have the Islandora Workbench Integration
         # module installed. So in effect, this test doesn't even test the term ID validation
-        # feature.
+        # feature. Issue about the best test here is at https://github.com/mjordan/islandora_workbench/issues/91.
         self.assertRegex(lines[-1], 'Configuration and input data appear to be valid', '')
 
 
