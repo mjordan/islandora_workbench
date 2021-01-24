2ae9a8304178e6ec8ba2272e570388a580f81182
Mark Jordan <mjordan@sfu.ca>
2021-01-24 14:44:20 -0800

    WIP on #209.

--- scripts/get_islandora_7_content.py
+++ scripts/get_islandora_7_content.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 
-'''Script for https://github.com/mjordan/islandora_workbench/issues/209.
+'''Script for exporting Islandora 7 content (metadata and OBJ datastreams).
+   Note: this script currently does not include sequence numbers for child
+   objects. See https://github.com/mjordan/islandora_workbench/issues/210.
 '''
 
 import os
 import re
 import requests
 import mimetypes
+import logging
 from progress_bar import InitBar
 
+############################
+# Configuration variables. #
+############################
 
 # Change to False to only fetch CSV metadata.
 fetch_files = True
@@ -19,6 +25,7 @@ islandora_base_url = 'http://localhost:8000'
 csv_output_path = 'islandora7_metadata.csv'
 # Will be created if it doesn't exist.
 obj_directory = '/tmp/objs'
+log_file_path = 'islandora_content.log'
 
 # Solr filter criteria. 'namespace' allows you to limit the metadata retrieved
 # from Solr to be limtited to objects with a specific namespace (or namespaces
@@ -36,11 +43,13 @@ field_pattern = 'mods_.*(_s|_ms)$'
 field_pattern_do_not_want = '(SFU_custom_metadata|marcrelator)'
 # 'standard_fields' is a list of fieldnames we always want in fields list. They are
 # added added to the field list after list is filtered down using 'field_pattern'.
-# @todo: Add compound properties.
-standard_fields = ['RELS_EXT_isMemberOfCollection_uri_ms', 'RELS_EXT_hasModel_uri_s', 'PID']
+# Columns for these fields will appear at the start of the CSV.
+standard_fields = ['PID', 'RELS_EXT_hasModel_uri_s', 'RELS_EXT_isMemberOfCollection_uri_ms', 'RELS_EXT_isConstituentOf_uri_ms']
 
 
-# Functions.
+##############
+# Functions. #
+##############
 
 def get_extension_from_mimetype(mimetype):
     # @todo: add_type() is not working, e.g. mimetypes.add_type('image/jpeg', '.jpg')
@@ -59,28 +68,45 @@ def get_percentage(part, whole):
     return 100 * float(part) / float(whole)
 
 
-# Main program logic.
+#######################
+# Main program logic. #
+#######################
+
+logging.basicConfig(
+    filename=log_file_path,
+    level=logging.INFO,
+    filemode='a',
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%d-%b-%y %H:%M:%S')
 
 # This query gets all fields in the index. Does not need to be user-configurable.
 fields_solr_query = '/select?q=*:*&wt=csv&rows=0&fl=*'
-fields_solr_request = solr_base_url + fields_solr_query
+fields_solr_url = solr_base_url + fields_solr_query
 
 # Get field list from Solr, filter it. The field list is then used in the query
 # to Solr to get the CSV data from Solr.
-field_list_response = requests.get(url=fields_solr_request, allow_redirects=True)
-raw_field_list = field_list_response.content.decode()
+try:
+    field_list_response = requests.get(url=fields_solr_url, allow_redirects=True)
+    raw_field_list = field_list_response.content.decode()
+except requests.exceptions.RequestException as e:
+    raise SystemExit(e)
+
 
 field_list = raw_field_list.split(',')
 filtered_field_list = [keep for keep in field_list if re.search(field_pattern, keep)]
 filtered_field_list = [discard for discard in filtered_field_list if not re.search(field_pattern_do_not_want, discard)]
 
+standard_fields.reverse()
 for standard_field in standard_fields:
     filtered_field_list.insert(0, standard_field)
 fields_param = ','.join(filtered_field_list)
 
 # Get the populated CSV from Solr, with the object namespace and field list filters applied.
 metadata_solr_request = solr_base_url + '/select?q=PID:' + namespace + '*&wt=csv&rows=1000000&fl=' + fields_param
-metadata_solr_response = requests.get(url=metadata_solr_request, allow_redirects=True)
+try:
+    metadata_solr_response = requests.get(url=metadata_solr_request, allow_redirects=True)
+except requests.exceptions.RequestException as e:
+    raise SystemExit(e)
 
 csv_output = list()
 # csv_header_row = ','.join(filtered_field_list)
@@ -91,7 +117,6 @@ if fetch_files is True:
     if not os.path.exists(obj_directory):
 	    os.makedirs(obj_directory)
 
-# @todo: Add exception handling for requests, log problems.
 row_count = 0
 pbar = InitBar()
 csv_header_row = rows.pop(0)
@@ -103,17 +128,23 @@ for row in rows:
         row_count += 1
         row_position = get_percentage(row_count, num_csv_rows)
         pbar(row_position)
-        obj_download_response = requests.get(url=obj_url, allow_redirects=True)
-        if obj_download_response.status_code == 200:
-            # Get MIMETYPE from 'Content-Type' header
-            obj_mimetype = obj_download_response.headers['content-type']
-            obj_extension = get_extension_from_mimetype(obj_mimetype)
-            obj_filename = pid.replace(':', '_')
-            obj_basename = obj_filename + obj_extension
-            # Save to file with name based on PID and extension based on MIMETYPE
-            obj_file_path = os.path.join(obj_directory, obj_basename)
-            open(obj_file_path, 'wb+').write(obj_download_response.content)
-            row = obj_basename + ',' + row
+        try:
+            obj_download_response = requests.get(url=obj_url, allow_redirects=True)
+            if obj_download_response.status_code == 200:
+	            # Get MIMETYPE from 'Content-Type' header
+	            obj_mimetype = obj_download_response.headers['content-type']
+	            obj_extension = get_extension_from_mimetype(obj_mimetype)
+	            obj_filename = pid.replace(':', '_')
+	            obj_basename = obj_filename + obj_extension
+	            # Save to file with name based on PID and extension based on MIMETYPE
+	            obj_file_path = os.path.join(obj_directory, obj_basename)
+	            open(obj_file_path, 'wb+').write(obj_download_response.content)
+	            row = obj_basename + ',' + row
+            if obj_download_response.status_code == 404:
+                logging.warning(obj_url + " not found.")
+        except requests.exceptions.RequestException as e:
+            logging.info(e)
+            continue
     else:
         # If we're not fetching files, add an empty file' column.
         row = ',' + row
