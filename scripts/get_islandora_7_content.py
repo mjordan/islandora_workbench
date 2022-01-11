60e1afec82cfe694754c17ea1288e8dc766743d4
ajstanley <alanjarlathstanley@gmail.com>
2022-01-10 20:57:32 -0400

    used CSV classes for data integrity

--- scripts/get_islandora_7_content.py
+++ scripts/get_islandora_7_content.py
@@ -112,10 +112,16 @@ try:
 except requests.exceptions.RequestException as e:
     raise SystemExit(e)
 
-csv_output = list()
 rows = metadata_solr_response.content.decode().splitlines()
+reader = csv.DictReader(rows)
+headers = reader.fieldnames
 # We add a 'sequence' column to store the Islandora 7.x property "isSequenceNumberOfxxx"/"isSequenceNumber".
-rows[0] = f"file,{rows[0]},sequence"
+headers.append('sequence')
+# Add a column to store the files
+headers.append('file')
+if config['id_field'] not in headers:
+    headers.append(config['id_field'])
+    index = config['id_start_number']
 
 if config['fetch_files'] is True:
     if not os.path.exists(config['obj_directory']):
@@ -123,53 +129,39 @@ if config['fetch_files'] is True:
 
 row_count = 0
 pbar = InitBar()
-csv_header_row = rows.pop(0)
 num_csv_rows = len(rows)
-for row in rows:
-    pid = row.split(',')[0]
-    sequence_number = get_child_sequence_number(pid)
-    row = row + ',' + str(sequence_number)
-    if config['fetch_files'] is True:
-        obj_url = config['islandora_base_url'] + '/islandora/object/' + pid + '/datastream/OBJ/download'
-        row_count += 1
-        row_position = get_percentage(row_count, num_csv_rows)
-        pbar(row_position)
-        try:
-            obj_download_response = requests.get(url=obj_url, allow_redirects=True)
-            if obj_download_response.status_code == 200:
-                # Get MIMETYPE from 'Content-Type' header
-                obj_mimetype = obj_download_response.headers['content-type']
-                obj_extension = utils.get_extension_from_mimetype(obj_mimetype)
-                obj_filename = pid.replace(':', '_')
-                obj_basename = obj_filename + obj_extension
-                # Save to file with name based on PID and extension based on MIMETYPE
-                obj_file_path = os.path.join(config['obj_directory'], obj_basename)
-                open(obj_file_path, 'wb+').write(obj_download_response.content)
-                row = obj_basename + ',' + row
-            if obj_download_response.status_code == 404:
-                logging.warning(obj_url + " not found.")
-                row = ',' + row
-        except requests.exceptions.RequestException as e:
-            logging.info(e)
-            continue
-    else:
-        # If we're not fetching files, add an empty 'file' column.
-        row = ',' + row
-
-    csv_output.append(row)
-
-# Write the CSV file.
-fields = csv_header_row.split(',')
-if config['id_field'] not in fields:
-    csv_header_row = f"{config['id_field']},{csv_header_row}"
-    for index, value in enumerate(csv_output):
-        csv_output[index] = f"{index + config['id_start_number']},{value}"
-
 with open(config['csv_output_path'], 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile, delimiter=',',
-                        quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    writer.writerow(csv_header_row.split(','))
-    for row in csv_output:
-        writer.writerow(row.split(','))
+    writer = csv.DictWriter(csvfile, fieldnames=headers)
+    writer.writeheader()
+    for row in reader:
+        sequence_number = get_child_sequence_number(row['PID'])
+        row['sequence'] = str(sequence_number)
+        if config['fetch_files'] is True:
+            obj_url = config['islandora_base_url'] + '/islandora/object/' + row['PID'] + '/datastream/OBJ/download'
+            row_count += 1
+            row_position = get_percentage(row_count, num_csv_rows)
+            pbar(row_position)
+            try:
+                obj_download_response = requests.get(url=obj_url, allow_redirects=True)
+                if obj_download_response.status_code == 200:
+                    # Get MIMETYPE from 'Content-Type' header
+                    obj_mimetype = obj_download_response.headers['content-type']
+                    obj_extension = utils.get_extension_from_mimetype(obj_mimetype)
+                    obj_filename = row['PID'].replace(':', '_')
+                    obj_basename = obj_filename + obj_extension
+                    # Save to file with name based on PID and extension based on MIMETYPE
+                    obj_file_path = os.path.join(config['obj_directory'], obj_basename)
+                    open(obj_file_path, 'wb+').write(obj_download_response.content)
+                    row['file'] = obj_basename
+                if obj_download_response.status_code == 404:
+                    logging.warning(obj_url + " not found.")
+
+            except requests.exceptions.RequestException as e:
+                logging.info(e)
+                continue
+
+            if config['id_field'] in headers:
+                row[config['id_field']] = index + reader.line_num - 2
+            writer.writerow(row)
 
 pbar(100)
