09e9f531487b76a4096ac2c37e0e933908856e90
Mark Jordan <mjordan@sfu.ca>
2022-07-26 07:25:47 -0700

    Changed license in setup.py.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email="mjordan@sfu",
     description="A command-line tool that allows creation, updating, and deletion of Islandora content.",
     url="https://github.com/mjordan/islandora_workbench",
-    license="The Unlicense",
+    license="MIT",
     install_requires=['requests>=2.22,<3', 'requests_cache', 'iteration_utilities', 'ruamel.yaml', 'progress_bar', 'openpyxl', 'unidecode', 'edtf_validate', 'rich'],
     python_requires='>=3.7',
     py_modules=[]
