ae1937b3b54c568a34d3660386009e9cf32b33b1
Yamil Suarez <github@yamil.com>
2023-08-24 09:20:45 -0400

    Create a separate file for new unit tests

---
+++ tests/unit_tests_workbench_config.py
@@ -0,0 +1,19 @@
+
+import unittest
+import WorkbenchConfig
+
+
+"""
+TODO
+
+* Need to figure out how to test things. May need to load up my own version of a config file that I then compare to the
+default config values???
+
+* need to instantiate an instance of the WorkbenchConfig to test it
+  
+"""
+
+class Test(unittest.TestCase):
+    # def test_get_config(selfself):
+    #     ...
+
