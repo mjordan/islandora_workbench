8b4ef7115a494501e8d0697df592fabecccaea40
Yamil Suarez <github@yamil.com>
2023-09-01 00:41:21 -0400

    Add test_path_check_valid_file()

--- tests/unit_tests_workbench_config.py
+++ tests/unit_tests_workbench_config.py
@@ -1,19 +1,34 @@
-
 import unittest
-import WorkbenchConfig
+import argparse
+from WorkbenchConfig import WorkbenchConfig
 
-
-"""
-TODO
-
-* Need to figure out how to test things. May need to load up my own version of a config file that I then compare to the
-default config values???
-
-* need to instantiate an instance of the WorkbenchConfig to test it
-  
-"""
 
 class Test(unittest.TestCase):
-    # def test_get_config(selfself):
+
+    def test_path_check_valid_file(self):
+        test_file_name = '/file/does/not/exist.yml'
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--config', required=True, help='Configuration file to use.')
+        args = parser.parse_args(['--config', test_file_name])
+
+        with self.assertRaises(SystemExit) as exit_return:
+            WorkbenchConfig(args)
+
+        error_message = 'Error: Configuration file "' + test_file_name + '" not found.'
+        self.assertEqual(exit_return.exception.code, error_message)
+
+    # def test_path_check_fail(self):
+    #     test_file_name = 'tests/assets/execute_bootstrap_script_test/config.yml'
+    #     parser = argparse.ArgumentParser()
+    #     parser.add_argument('--config', required=True, help='Configuration file to use.')
+    #     args = parser.parse_args(['--config', test_file_name], '--check')
+    #
+    #     with self.assertRaises(SystemExit) as exit_return:
+    #         WorkbenchConfig(args)
+    #
+    #     error_message = 'Error: Configuration file "' + test_file_name + '" not found.'
+    #     self.assertEqual(exit_return.exception.code, error_message)
+
+    # def test_get_config(self):
     #     ...
 
