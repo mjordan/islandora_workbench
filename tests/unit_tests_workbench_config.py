1fef346435ec97291e9aa02e9e97f4e50507d1fb
Yamil Suarez <github@yamil.com>
2023-09-02 23:54:03 -0400

    Add mocks for logging to test_path_check_valid_file()

--- tests/unit_tests_workbench_config.py
+++ tests/unit_tests_workbench_config.py
@@ -1,5 +1,7 @@
 import unittest
+from unittest.mock import patch
 import argparse
+
 from WorkbenchConfig import WorkbenchConfig
 
 
@@ -11,11 +13,17 @@ class Test(unittest.TestCase):
         parser.add_argument('--config', required=True, help='Configuration file to use.')
         args = parser.parse_args(['--config', test_file_name])
 
-        with self.assertRaises(SystemExit) as exit_return:
+        with self.assertRaises(SystemExit) as exit_return,\
+                patch('WorkbenchConfig.logging') as mocked_logging:
+
+            mocked_logging.return_value = None
+
             WorkbenchConfig(args)
 
         error_message = 'Error: Configuration file "' + test_file_name + '" not found.'
         self.assertEqual(exit_return.exception.code, error_message)
+
+        # TODO: check values sent to logger
 
     # def test_path_check_fail(self):
     #     test_file_name = 'tests/assets/execute_bootstrap_script_test/config.yml'
