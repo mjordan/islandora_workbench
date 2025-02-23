ee2661f748520725339622e11f58e79316478fff
Mark Jordan <mjordan@sfu.ca>
2025-02-23 13:31:13 -0800

    WIP on #899.

--- WorkbenchConfig.py
+++ WorkbenchConfig.py
@@ -222,6 +222,7 @@ class WorkbenchConfig:
             "log_term_creation": True,
             "log_file_name_and_line_number": False,
             "progress_bar": False,
+            "show_percentage_of_csv_input_processed": False,
             "user_agent": "Islandora Workbench",
             "allow_redirects": True,
             "secure_ssl_only": True,
