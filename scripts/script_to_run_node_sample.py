c064c6fa23195e426fc29db95c71423cc2f84bf5
Mark Jordan <mjordan@sfu.ca>
2025-07-02 08:05:09 -0700

    WIP on #979.

--- scripts/script_to_run_node_sample.py
+++ scripts/script_to_run_node_sample.py
@@ -8,13 +8,23 @@ import json
 import requests
 from ruamel.yaml import YAML
 
+# All scripts registered in "run_scripts" are passed the path to the Workbench config file
+# and a single entity ID, in that order.
 config_file = sys.argv[1]
 node_id = sys.argv[2]
 
+# Note that like hook scripts, scripts registered in "run_scripts" only have access
+# to configuration values defined in the Workbench config file, not to default values.
+# Therefore, if you want to reuse the Workbench config file in your scripts, the config
+# file should include settings that would otherwise have the default values.
 yaml = YAML()
 with open(config_file, "r") as stream:
     config = yaml.load(stream)
 
+# Scripts should do their own logging, although if the Workbench config setting is
+# "run_scripts_log_script_output" is set to true (its default value), the output
+# of your scripts will be logged in the Workbench log file. Set "run_scripts_log_script_output"
+# to false to omit scripts' output from the Workbench log.
 logging.basicConfig(
     filename="script_to_run_sample.log",
     level=logging.INFO,
@@ -24,7 +34,8 @@ logging.basicConfig(
 
 requests.packages.urllib3.disable_warnings()
 
-# Get each node's title and print/log it.
+# The demonstration purpose of this script is to fetch each node's title and print/log it,
+# using the "host" setting defined in the Workbench config file.
 url = f'{config["host"].rstrip("/")}/node/{node_id}?_format=json'
 result = requests.get(url, verify=False)
 try:
@@ -36,4 +47,5 @@ except Exception as e:
     logging.error(
         f"Could not retrieve data from {url}, HTTP response code was {result.status_code} {e}."
     )
+    # Scripts should always exit with a non-0 code on failure so Workbench can detect the failure.
     sys.exit(1)
