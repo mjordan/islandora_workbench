4b8c1b0247d5fef748b142a0e9d40160aeaf3b37
Mark Jordan <mjordan@sfu.ca>
2026-04-28 07:04:52 -0700

    WIP on #1087.

--- scripts/encrypt_credentials_file.py
+++ scripts/encrypt_credentials_file.py
@@ -5,12 +5,12 @@ credentials_file = sys.argv[1]
 key = Fernet.generate_key()
 fernet = Fernet(key)
 
-with open(credentials_file, 'rb') as f:
+with open(credentials_file, "rb") as f:
     plain_text = f.read()
 
 encrypted = fernet.encrypt(plain_text)
 
-with open(credentials_file, 'wb') as f:
+with open(credentials_file, "wb") as f:
     f.write(encrypted)
 
-print(f"\nThe encryption key for '{credentials_file}' is {key.decode()}\n")
+print(f"'{credentials_file}' can be decrypted using the key {key.decode()}")
